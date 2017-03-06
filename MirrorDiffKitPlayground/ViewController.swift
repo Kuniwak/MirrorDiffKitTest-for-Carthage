1ebf71bcdb5b07d6240547e7f5f415ac6a3e6fb5
Kuniwak <orga.chem.job@gmail.com>
2017-03-06 21:26:08 +0900

    Hello, Carthage

---
+++ MirrorDiffKitPlayground/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MirrorDiffKitPlayground
+//
+//  Created by yuki.kokubun on 2017/03/06.
+//  Copyright (c) 2017 Kuniwak. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
